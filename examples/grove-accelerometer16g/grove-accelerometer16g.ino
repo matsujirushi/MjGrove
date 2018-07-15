fe18dd99f4f370921f65f42ef4276da8ea209361
Takashi Matsuoka <matsujirushi@live.jp>
2018-07-15 16:32:00 +0900

    Modified sample code

--- examples/grove-accelerometer16g/grove-accelerometer16g.ino
+++ examples/grove-accelerometer16g/grove-accelerometer16g.ino
@@ -14,14 +14,22 @@
 #define REG_POWER_CTL (0x2d)
 #define REG_DATAX0    (0x32)
 
+Wio3G Wio;
 GroveBoard Board;
+GroveI2CDevice* dev;
 
 void setup() {
+  delay(200);
+  
   SerialUSB.begin(115200);
+
+  Wio.Init();
+  Wio.PowerSupplyGrove(true);
+  delay(500);
 
   WireI2C.begin();
   
-  GroveI2CDevice* dev = Board.I2C.NewGroveI2CDevice(I2C_ADDRESS);
+  dev = Board.I2C.NewGroveI2CDevice(I2C_ADDRESS);
   
   uint8_t data[2];
   data[0] = REG_POWER_CTL;
@@ -30,6 +38,22 @@ void setup() {
 }
 
 void loop() {
+  uint8_t writeData[1];
+  writeData[0] = REG_DATAX0;
+  dev->Write(writeData, sizeof(writeData));
+
+  uint8_t readData[6];
+  if (dev->Read(readData, sizeof(readData)) == 6)
+  {
+    float x = (*(int16_t*)&readData[0]) * 2.0 / 512;
+    float y = (*(int16_t*)&readData[2]) * 2.0 / 512;
+    float z = (*(int16_t*)&readData[4]) * 2.0 / 512;
+    SerialUSB.print(x);
+    SerialUSB.print(' ');
+    SerialUSB.print(y);
+    SerialUSB.print(' ');
+    SerialUSB.println(z);
+  }
+
   delay(INTERVAL);
 }
-
