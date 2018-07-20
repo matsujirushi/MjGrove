15c6a4b2c9b852b42e85f52d03edb797de85864a
Takashi Matsuoka <matsujirushi@live.jp>
2018-07-20 21:36:42 +0900

    Don't use HalRcc and HalGpio

--- src/Interface/GroveGpio.h
+++ src/Interface/GroveGpio.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#if defined ARDUINO_STM32F4_WIO_GPS
-
 #include <Arduino.h>
 
 class GroveGpio
@@ -57,64 +55,3 @@ public:
 	}
 
 };
-
-#elif defined ARDUINO_WIO_3G
-
-#include "../HAL/HalGpio.h"
-
-class GroveGpio : private HalGpio
-{
-public:
-	enum ModeType
-	{
-		MODE_INPUT,
-		MODE_OUTPUT,
-	};
-
-public:
-	GroveGpio(int pin) : HalGpio(pin)
-	{
-	}
-
-	void Enable()
-	{
-		HalGpio::Enable();
-	}
-
-	void SetMode(ModeType mode)
-	{
-		switch (mode)
-		{
-		case MODE_INPUT:
-			SetPullUpDown(HalGpio::PUPD_NO);
-			HalGpio::SetMode(HalGpio::MODE_INPUT);
-			break;
-		case MODE_OUTPUT:
-			SetPullUpDown(HalGpio::PUPD_NO);
-			SetOutType(HalGpio::OUT_TYPE_PUSH_PULL);
-			SetOutSpeed(HalGpio::OUT_SPEED_VERY_HIGH);
-			HalGpio::SetMode(HalGpio::MODE_OUTPUT);
-			break;
-		}
-	}
-
-	void Write(bool on)
-	{
-		HalGpio::Write(on);
-	}
-
-	bool Read() const
-	{
-		return HalGpio::Read();
-	}
-
-	bool ReadOutput() const
-	{
-		return HalGpio::ReadOutput();
-	}
-
-};
-
-#else
-#error "This board is not supported."
-#endif
