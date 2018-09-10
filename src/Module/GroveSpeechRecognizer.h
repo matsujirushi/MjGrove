7540763b3a8d2a44bd754c543dd1c5bae7136d36
Takashi Matsuoka <matsujirushi@live.jp>
2018-09-10 22:09:02 +0900

    Update Grove - Speech Recognizer (develop)

--- src/Module/GroveSpeechRecognizer.h
+++ src/Module/GroveSpeechRecognizer.h
@@ -10,6 +10,33 @@
 
 class GroveSpeechRecognizer : public GroveModule
 {
+public:
+	enum COMMAND_TYPE
+	{
+		TURN_ON_THE_LIGHT,
+		TURN_OFF_THE_LIGHT,
+		PLAY_MUSIC,
+		PAUSE,
+		NEXT,
+		PREVIOUS,
+		UP,
+		DOWN,
+		TURN_ON_THE_TV,
+		TURN_OFF_THE_TV,
+		INCREASE_TEMPERATURE,
+		DECREASE_TEMPERATURE,
+		WHATS_THE_TIME,
+		OPEN_THE_DOOR,
+		CLOSE_THE_DOOR,
+		LEFT,
+		RIGHT,
+		STOP,
+		START,
+		MODE_1,
+		MODE_2,
+		GO,
+	};
+
 private:
 	HalUART* _UART;
 	void(*_MessageReceivedCallback)(const char* message);
