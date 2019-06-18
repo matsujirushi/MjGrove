7b4d2c0b03a1ba5ef35a539475c9ae957d70d75a
のびすけ <rsksound@gmail.com>
2019-06-19 01:25:19 +0900

    BG96 Grove Tilt Example追加

---
+++ examples/GroveTilt/GroveTilt.ino
@@ -0,0 +1,24 @@
+// BOARD Seeed Wio 3G
+// GROVE D38 <-> Grove - Tilt (SKU#101020025)
+
+#include <MjGrove.h>
+
+#define INTERVAL    (100)
+
+GroveBoard Board;
+GroveTilt Tilt(&Board.D38);
+
+void setup() {
+  delay(200);
+  SerialUSB.begin(115200);
+
+  Board.D38.Enable();
+  Tilt.Init();
+}
+
+void loop() {
+  bool tiltState = Tilt.IsOn();
+  SerialUSB.print(tiltState ? '*' : '.');
+  
+  delay(INTERVAL);
+}
